e4a683a69acb0c5bac5fea5ab4bb069d384075b3
lucasrguerra <l.rayanguerra@gmail.com>
2025-01-23 03:46:52 -0300

    Update padding in index.html

--- main.py
+++ main.py
@@ -67,4 +67,4 @@ def to_stl(request: requests.ToSTLRequest):
         raise HTTPException(status_code=500, detail=str(e))
     
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
